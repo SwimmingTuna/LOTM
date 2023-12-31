04916752c3866cf694cfe74c8a8315e1f937be68
BuildTools <unconfigured@null.spigotmc.org>
2023-12-30 21:36:44 -0500

    Test

---
+++ src/main/java/net/swimmingtuna/lotm/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.swimmingtuna.lotm.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.swimmingtuna.lotm.LOTM;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, LOTM.MOD_ID);
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
